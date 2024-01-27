7184479344553018324a66bacde1d88073f7249a
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2024-01-27 12:38:16 +0200

    CLI lib update names

--- Firmware/DigitalResistor_py/SendBox/Test_cli_class.py
+++ Firmware/DigitalResistor_py/SendBox/Test_cli_class.py
@@ -1,4 +1,4 @@
-import CLI_class
+import simplePyCLI
 
 def turn_on_led(param, param2):
     # Custom action to turn on the LED based on the parameter
@@ -9,7 +9,7 @@ def turn_off_led(param, param2):
     # Custom action to turn off the LED based on the parameter
     print("Led Off")
 
-cli = CLI_class.simplePyCLI(">")
+cli = simplePyCLI.simplePyCLI(">")
 cmd_help = "turning led on "
 cli.add_command('led_on', turn_on_led, 2, cmd_help)  # Add a custom command "led_on" with the corresponding action
 cmd_help = "turning led off "
